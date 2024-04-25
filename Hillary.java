d93ef3fbe4a142119cb259c815375a6ad6cf6ada
hanawe1 <131805297+hanawe1@users.noreply.github.com>
2024-04-25 12:47:12 -0300

    Add files via upload

---
+++ Hillary.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Hillary {
+    public static void main(String[] args) {
+        String nome = "Hillary Hanawe";
+        int idade = 17;
+        String complemento = " anos";
+
+        System.out.println("Minha biografia:");
+        System.out.println("Meu nome é: " + nome);
+        System.out.println("Eu tenho: " + idade + complemento);
+
+    }
+}
